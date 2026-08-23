19637b3bb62d0149f59dcab398b815de33873f64
Michael Jonker <michael@openpoint.ie>
2026-08-23 02:44:37 +0100

    Fix license header

--- src/hash.ts
+++ src/hash.ts
@@ -1,21 +1,21 @@
-export function hash(targets: fm.opts.target<fm.kind>[]) {
-  /*
-   * Copyright (C) 2026 Michael Jonker
-   *
-   * This program is free software: you can redistribute it and/or modify
-   * it under the terms of the GNU General Public License as published by
-   * the Free Software Foundation, either version 3 of the License, or
-   * (at your option) any later version.
-   *
-   * This program is distributed in the hope that it will be useful,
-   * but WITHOUT ANY WARRANTY; without even the implied warranty of
-   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   * GNU General Public License for more details.
-   *
-   * You should have received a copy of the GNU General Public License
-   * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-   */
+/*
+ * Copyright (C) 2026 Michael Jonker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
+export function hash(targets: fm.opts.target<fm.kind>[]) {
   const target_keys = parse_targets(targets);
   const str = target_keys.join("");
   let h1 = 0;
